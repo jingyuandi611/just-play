cc6be61500ab7883100c8a0270719c7b1bb25b4c
jingyuandi611 <15926461748@163.com>
2018-05-01 20:57:58 +0800

    菜单开发

--- src/layout/layout.module.ts
+++ src/layout/layout.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core/src/metadata/ng_module";
-import { LayoutCompomemt} from './layout.component'
+import { LayoutCompomemt } from './layout.component'
+import { NzMenuModule } from 'ng-zorro-antd';
 
 @NgModule({
-    imports: [LayoutCompomemt],
+    imports: [
+        LayoutCompomemt,
+        NzMenuModule
+    ],
     declarations: [LayoutCompomemt]
 })
 
